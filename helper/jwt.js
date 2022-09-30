888d57a3ee6117017b0164db77c3afbe86e591e0
Aimeeumuhoza <aimeeumuhoza1@gmail.com>
2022-09-30 16:53:59 +0200

    changes

--- helper/jwt.js
+++ helper/jwt.js
@@ -1,6 +1,8 @@
 const  jwt = require("jsonwebtoken")
+const  dotenv = require("dotenv")
 
+dotenv.config()
 const sign = (data)=> jwt.sign({data},process.env.SECRET_KEY,{expiresIn: '72h'})
-const verify = (data) => jwt.verify(data,process.env.SECRET_KEY)
+const verify = (data) => jwt.verify(data, process.env.SECRET_KEY)
 
 module.exports = {sign, verify}
--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,23 +1,28 @@
 const {verify} = require("../helper/jwt")
+const  jwt = require("jsonwebtoken")
+const dotenv = require("dotenv")
 
+dotenv.config()
 const verifyToken = (req,res,next)=>{
    const token =  req.headers.authorization
 
-   const user = verify(token)
+   const user = jwt.verify(token, process.env.SECRET_KEY)
    if(!user){
      return res.status(400).json({message:"token is required"})
    }
    req.token =token
    req.user = user
-  next()
+   console.log(req.user)
+  
 }
 
 const verifyTokenAndAdmin = async(req,res,next) => {
 
   const token =  req.headers.authorization
   const user = verify(token)
-    if(user.role === "admin"){
-      next()
+  console.log(user.data.role);
+    if(user.data.role === "admin"){
+     next()
     }else{
       res.status(500).json("you don't have access")
     }
@@ -27,7 +32,8 @@ const verifyTokenAndAdmin = async(req,res,next) => {
 const verifyTokenAndUser = (req,res,next)=>{
   const token =  req.headers.authorization
   const user = verify(token)
-    if(user._id === req.params._id){
+
+    if(user.data._id === req.params._id || "client"){
       next()
     }else{
       res.status(500).json("you don't have access")
